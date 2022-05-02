b7671a3f04d87897d144433d72a4986dfb1f40fb
av.litvin <av.litvin73@gmail.com>
2022-05-02 23:16:57 +0300

    feat: Binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,132 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js')
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.treeRoot = null
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.treeRoot
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let addNodeData = function (node, data) {
+      if(!node){
+        return new Node(data)
+      }
+      if(node.data < data){
+        node.right = addNodeData(node.right, data)
+      } else if (node.data > data){
+        node.left = addNodeData(node.left, data)
+      }
+      if(node.data === data){
+        return node
+      }
+      return node
+    }
+
+    this.treeRoot = addNodeData(this.treeRoot, data)
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let searchNode = function(node, data){
+      if(!node){
+        return false
+      }
+      if(node.data < data){
+        return searchNode(node.right, data)
+      } else if(node.data > data){
+       return searchNode(node.left, data)
+      }
+      
+      if(node.data === data){
+        return true
+      }
+    }
+
+    return searchNode(this.treeRoot, data)
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let searchNode = function(node, data){
+      if(!node){
+        return null
+      }
+      if (node.data < data){
+        return searchNode(node.right, data)
+      } else if (node.data > data){
+        return searchNode(node.left, data)
+      }
+      if (node.data === data){
+        return node
+      }
+    }
+    return searchNode(this.treeRoot, data)
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    function removeNode(node, data){
+      if (!node) {
+        return null
+      }
+      if (data < node.data) {
+        node.left = removeNode(node.left, data)
+        return node
+      } else if (node.data < data) {
+        node.right = removeNode(node.right, data)
+        return node
+      } else {
+        if (!node.left && !node.right) {
+          return null
+        }
+        if (!node.left) {
+          node = node.right
+          return node
+        }
+        if (!node.right) {
+          node = node.left
+          return node
+        }
+        let minFromRight = node.right
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left
+        }
+        node.data = minFromRight.data
+        node.right = removeNode(node.right, minFromRight.data)
+      }return node
+    }
+    this.treeRoot = removeNode(this.treeRoot, data)
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.treeRoot) {
+      return null
+    }
+    let node = this.treeRoot
+    while (node.left) {
+      node = node.left
+    }
+    return node.data
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.treeRoot) {
+      return null
+    }
+    let node = this.treeRoot
+    while (node.right) {
+      node = node.right
+    }
+    return node.data
   }
 }
 
 module.exports = {
   BinarySearchTree
-};
+}
