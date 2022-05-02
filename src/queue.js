dc12b22ddd9f0de1a32bd70e167accc5b70abde7
av.litvin <av.litvin73@gmail.com>
2022-05-02 23:40:58 +0300

    feat: Queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,21 +14,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.tail = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head
+  }
+
+  enqueue(value) {
+    let addToTheEnd = function(node, value){
+      if (!node){
+        return new ListNode(value)
+      }
+      if(!node.value){
+        node.value = value
+      } else {
+        node.next = addToTheEnd(node.next, value)
+      }
+      return node
+    }
+    this.head = addToTheEnd(this.head, value)
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let value = this.head.value;
+    this.head = this.head.next;
+    return value;
+    }
 }
 
 module.exports = {
